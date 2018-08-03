2738bbe0e2e44bec52ad808168325f075b560149
KimFlowering <kyhyun1999@gmail.com>
2018-08-04 03:41:58 +0900

    Oh No

--- src/main/java/com/as/kim/controller/HomeController.java
+++ src/main/java/com/as/kim/controller/HomeController.java
@@ -2,29 +2,22 @@ package com.as.kim.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.tomcat.jni.Local;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.as.kim.service.BoardService;
 import com.as.kim.service.LoginService;
-import com.as.kim.service.dao.LoginDAO;
-import com.as.kim.vo.BoardVO;
 import com.as.kim.vo.LoginVO;
 
 /**
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,14 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@ page contentType="text/html;charset=UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
 <html>
 <head>
-	<title>Home</title>
+<meta charset="UTF-8">
+<title>Home</title>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
-
-<P>  The time on the server is ${serverTime}. </P>
+	<p> 개발자 김용현입니다. </p>
+	<button>로그인</button>
+	<button onclick="location.href='/member/createAccount.jsp'">회원가입</button>
 </body>
 </html>
